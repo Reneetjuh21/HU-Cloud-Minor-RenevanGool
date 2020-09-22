14acfb81d6b524a9b3b8b26cd50fca8650ae1fc0
René van Gool <r.vangool@live.nl>
2020-09-22 19:19:30 +0200

    & added to sh for startup

--- runapp.sh
+++ runapp.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 echo 'Starting my app'
 cd '/home/ubuntu/server'
-java -jar cnsd17-backend-0.0.1-SNAPSHOT.jar
+java -jar cnsd17-backend-0.0.1-SNAPSHOT.jar -Dspring.profiles.active=prod &

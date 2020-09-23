e11913472c9959bd7f3e194ded813f68e9c2186f
René van Gool <r.vangool@live.nl>
2020-09-23 11:13:11 +0200

    Update runapp.sh

--- runapp.sh
+++ runapp.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 echo 'Starting my app'
 cd '/home/ubuntu/server'
-java -jar cnsd17-backend-0.0.1-SNAPSHOT.jar -Dspring.profiles.active=prod > /dev/null 2> /dev/null < /dev/null &
+java -jar cnsd17-backend-0.0.1-SNAPSHOT.jar --spring.profiles.active=prod > /dev/null 2> /dev/null < /dev/null &
